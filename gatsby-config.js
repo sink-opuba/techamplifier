beec9f2f06c15422852413f63ca4a020f6e87e22
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-03-05 12:50:23 +0100

    modify gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/waffiihub-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/waffiihub-icon.png`,
       },
     },
     {
